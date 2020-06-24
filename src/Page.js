19f69365d1b5e483f5816b14c4947e3b5147ff92
Rob Lark <quietcalmrecords@Robs-iMac.local>
2020-06-24 12:02:25 -0700

    completed with strecth goal

--- src/Page.js
+++ src/Page.js
@@ -6,12 +6,20 @@ import ImageHolder from "./ImageHolder";
 const Page = (props) => {
 	return (
 		<div>
-			<h1 style={{ textAlign: "center" }}>NASA PHOTO OF THE DAY</h1>
-			<Header title={props.image.title} />
-			<ParagraphMaker data={props.image.date} />
-			<ImageHolder src={props.image.hdurl} />
-			<ParagraphMaker data={props.image.explanation} />
+			<h1 style={{ textAlign: "center" }}>MARS ROVER PHOTO OF THE DAY</h1>
+			<Header title={props.image.full_name} />
+			<ParagraphMaker data={props.image.earth_date} />
+			<ImageHolder src={props.image.img_src} />
+			{/* <ParagraphMaker data={props.image.explanation} /> */}
 		</div>
+
+		// <div>
+		// 	<h1 style={{ textAlign: "center" }}>NASA PHOTO OF THE DAY</h1>
+		// 	<Header title={props.image.title} />
+		// 	<ParagraphMaker data={props.image.date} />
+		// 	<ImageHolder src={props.image.hdurl} />
+		// 	<ParagraphMaker data={props.image.explanation} />
+		// </div>
 	);
 };
 
--- src/PostsPage.js
+++ src/PostsPage.js
@@ -6,15 +6,34 @@ const PostsPage = () => {
 	const [image, setImage] = useState({});
 
 	useEffect(() => {
-		Axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
+		Axios.get(
+			`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY`
+		)
 			.then((results) => {
-				console.log("Results from useEffect of NasaAPI", results);
-				setImage(results.data);
-				console.log(results.data);
+				///getting an array of object Keys.
+				const goodImages = results.data.photos;
+				//generating a random #
+				const number = Math.random() * goodImages.length;
+				let indexNumber = Math.floor(number);
+				//assigning that random # as a key in the object
+				const objectID = goodImages[indexNumber];
+				console.log(objectID);
+
+				setImage(objectID);
 			})
 			.catch((err) => {
-				console.log("Error occured in useEffect of NasaAPI: ", err);
+				console.log("oh no!, ", err);
 			});
+
+		// Axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
+		// 	.then((results) => {
+		// 		console.log("Results from useEffect of NasaAPI", results);
+		// 		setImage(results.data);
+		// 		console.log(results.data);
+		// 	})
+		// 	.catch((err) => {
+		// 		console.log("Error occured in useEffect of NasaAPI: ", err);
+		// 	});
 	}, []);
 
 	return (
