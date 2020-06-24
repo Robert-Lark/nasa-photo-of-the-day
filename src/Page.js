24f8f8af7f717fd38af5de1e1af3b87049f387e1
Rob Lark <quietcalmrecords@Robs-iMac.local>
2020-06-24 10:52:20 -0700

    added h1 header

--- src/Page.js
+++ src/Page.js
@@ -6,6 +6,7 @@ import ImageHolder from "./ImageHolder";
 const Page = (props) => {
 	return (
 		<div>
+			<h1 style={{ textAlign: "center" }}>NASA PHOTO OF THE DAY</h1>
 			<Header title={props.image.title} />
 			<ParagraphMaker data={props.image.date} />
 			<ImageHolder src={props.image.hdurl} />
