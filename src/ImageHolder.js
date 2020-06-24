8dbe80ca92b4ab50697ef2a8891b8a59604577ea
Rob Lark <quietcalmrecords@Robs-iMac.local>
2020-06-24 10:44:29 -0700

    additional inline styling applied

--- src/ImageHolder.js
+++ src/ImageHolder.js
@@ -3,7 +3,12 @@ import React from "react";
 const ImageHolder = (props) => {
 	return (
 		<div>
-			<img src={props.src} alt="default" className="ofTheDayImg" />
+			<img
+				style={({ width: "100vw" }, { height: "100vh" })}
+				src={props.src}
+				alt="default"
+				className="ofTheDayImg"
+			/>
 		</div>
 	);
 };
